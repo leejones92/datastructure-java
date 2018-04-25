e828dd8d7b52fd115cdb67dea28994a43393d3f7
liyang <liyang@51dojo.com>
2018-04-25 11:32:51 +0800

    git init

---
+++ src/main/java/linear/LeeLinkedList.java
@@ -0,0 +1,10 @@
+package linear;
+
+/**
+ * @author liyang
+ * @create 2018-04-25 上午11:08
+ * @description
+ **/
+public class LeeLinkedList {
+
+}
---
+++ src/main/java/linear/LeeQueue.java
@@ -0,0 +1,10 @@
+package linear;
+
+/**
+ * @author liyang
+ * @create 2018-04-25 上午11:06
+ * @description
+ **/
+public class LeeQueue {
+
+}
